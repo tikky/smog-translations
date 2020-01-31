8c66381858f93872da568da9e558e812dbfad619
tikky <piotr.fuz@gmail.com>
2020-01-31 10:13:07 +0100

    first commit

---
+++ language/english/general_lang.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['hello'] = 'hello';
+
+$lang['more_information'] = 'More information';
+$lang['last_reading'] = 'Last reading';
+$lang['view_report'] = 'View Report';
+
+$lang['temperature'] = 'Temperature';
+$lang['humidity'] = 'Humidity';
+$lang['pressure'] = 'Pressure';
+$lang['temp_and_humm'] = 'Temperature and hummidity';
+$lang['weather_forecast'] = 'Weather forecast';
+$lang['last_24h'] = 'Last 24h';
+$lang['stations_map'] = 'Air Pollution Map';
+$lang['station_map'] = 'Station location';
+$lang['stations_list'] = 'Measuring points';
+
+$lang['error_current_data'] = 'No current reading from this measuring station.';
+$lang['error_possible_cause'] = 'Perhaps it is temporarily out of power or under maintenance.';
+$lang['error_last_reading'] = 'The following data shows the last reading.';
+$lang['error_warning'] = 'Warning';
+$lang['error'] = 'Error';
+$lang['average_data_per_hour'] = 'Average data per hour';
+$lang['atmospheric_factors'] = 'Atmospheric factors';
+$lang['average'] = 'Average';
+$lang['day_temperature'] = 'Day temp';
+$lang['night_temperature'] = 'Night Temp';
+$lang['particulate_matter'] = 'Particulate matter';
+$lang['today'] = 'Today';
+$lang['tomorrow'] = 'Tomorrow';
+$lang['how_we_measure'] = 'How we measure';
+$lang['search'] = 'Search';
+$lang['information'] = 'Information';
+$lang['of_norm'] = 'of the norm';
+$lang['pollution'] = 'Air pollution';
+$lang['last_added'] = 'Last added';
+
+//shortest as possible
+$lang['very_low'] = 'Very low';
+$lang['low'] = 'Low';
+$lang['medium'] = 'Medium';
+$lang['high'] = 'High';
+$lang['very_high'] = 'Very high';
+//
+
+$lang['accept'] = 'Accept';
+$lang['cancel'] = 'Cancel';
+$lang['yes'] = 'Yes';
+$lang['no'] = 'No';
+$lang['privacy_policy'] = 'Privacy policy';
+$lang['contact'] = 'Contact';
+$lang['about_company'] = 'About company';
---
+++ language/polish/general_lang.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['hello'] = 'cześć';
+
+$lang['more_information'] = 'Więcej informacji';
+$lang['last_reading'] = 'Ostatni odczyt';
+$lang['view_report'] = 'Zobacz raport';
+
+$lang['temperature'] = 'Temperatura';
+$lang['humidity'] = 'Wilgotność';
+$lang['pressure'] = 'Ciśnienie';
+$lang['temp_and_humm'] = 'Temperatura i wilgotność';
+$lang['weather_forecast'] = 'Prognoza pogody';
+$lang['last_24h'] = 'Ostatnie 24h';
+$lang['stations_map'] = 'Mapa stacji';
+$lang['station_map'] = 'Lokalizacja czuniknika';
+$lang['stations_list'] = 'Stacje pomiarowe';
+
+$lang['error_current_data'] = 'Brak aktualnego odczytu z tej stacji pomiarowej.';
+$lang['error_possible_cause'] = 'Być może chwilowo nie ma zasilania bądź jest w konserwacji.';
+$lang['error_last_reading'] = 'Poniższe dane pokazują ostatni odczyt.';
+$lang['error_warning'] = 'Uwaga';
+$lang['error'] = 'Błąd';
+$lang['average_data_per_hour'] = 'Uśrednione pomiary godzinowe';
+$lang['atmospheric_factors'] = 'Czynniki atmosferyczne';
+$lang['average'] = 'Średnia';
+$lang['day_temperature'] = 'Temp dzień';
+$lang['night_temperature'] = 'Temp noc';
+$lang['particulate_matter'] = 'Cząsteczki stałe';
+$lang['today'] = 'Dziś';
+$lang['tomorrow'] = 'Jutro';
+$lang['how_we_measure'] = 'Jak mierzymy';
+$lang['search'] = 'Szukaj';
+$lang['information'] = 'Informacje';
+$lang['of_norm'] = 'normy';
+$lang['pollution'] = 'Zanieczyszczenie';
+$lang['last_added'] = 'Ostatnio dodane';
+
+//Jak najkrótsze tłumacznenia poniżych pięciu
+$lang['very_low'] = 'Najniższy'; 
+$lang['low'] = 'Niski';
+$lang['medium'] = 'Średni';
+$lang['high'] = 'Wysoki';
+$lang['very_high'] = 'Najwyższy';
+//
+
+$lang['accept'] = 'Akceptuj';
+$lang['cancel'] = 'Anuluj';
+$lang['yes'] = 'Tak';
+$lang['no'] = 'Nie';
+$lang['privacy_policy'] = 'Polityka prywatności';
+$lang['contact'] = 'Kontakt'; //dział kontakt na stronie
+$lang['about_company'] = 'O firmie';
