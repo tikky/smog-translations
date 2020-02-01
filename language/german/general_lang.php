19702ed8b8405115d05392a3b346276aa34644cb
tikky <piotr.fuz@gmail.com>
2020-02-01 22:01:16 +0100

    First commit

---
+++ language/german/general_lang.php
@@ -0,0 +1,64 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['hello'] = 'Hallo';
+
+$lang['more_information'] = 'Weitere Informationen';
+$lang['last_reading'] = 'Letzte Lesung';
+$lang['view_report'] = 'Bericht anzeigen';
+
+$lang['temperature'] = 'Temperatur';
+$lang['humidity'] = 'Luftfeuchtigkeit';
+$lang['pressure'] = 'Druck';
+$lang['temp_and_humm'] = 'Temperatur und Luftfeuchtigkeit';
+$lang['weather_forecast'] = 'Wettervorhersage';
+$lang['last_24h'] = 'Letzte 24h';
+$lang['stations_map'] = 'Luftverschmutzungskarte';
+$lang['station_map'] = 'Standort der Station';
+$lang['stations_list'] = 'Messpunkte';
+
+$lang['error_current_data'] = 'Kein aktueller Messwert von dieser Messstation.';
+$lang['error_possible_cause'] = 'Möglicherweise ist der Strom vorübergehend ausgefallen oder wird gewartet.';
+$lang['error_last_reading'] = 'Die folgenden Daten zeigen den letzten Messwert.';
+$lang['error_warning'] = 'Warnung';
+$lang['error'] = 'Fehler';
+$lang['average_data_per_hour'] = 'Durchschnittliche Daten pro Stunde';
+$lang['atmospheric_factors'] = 'Atmosphärische Faktoren';
+$lang['average'] = 'Durchschnitt';
+$lang['day_temperature'] = 'Tagestemp';
+$lang['night_temperature'] = 'Nachttemp';
+$lang['particulate_matter'] = 'Feinstaub';
+$lang['today'] = 'Heute';
+$lang['tomorrow'] = 'Morgen';
+$lang['how_we_measure'] = 'Wie wir messen';
+$lang['search'] = 'Suche';
+$lang['information'] = 'Information';
+$lang['of_norm'] = 'der Norm';
+$lang['pollution'] = 'Luftverschmutzung';
+$lang['last_added'] = 'Zuletzt hinzugefügt';
+$lang['no_data'] = 'Keine Daten';
+
+// so kurz wie möglich
+$lang['very_low'] = 'Sehr niedrig';
+$lang['low'] = 'Niedrig';
+$lang['medium'] = 'Mittel';
+$lang['high'] = 'Hoch';
+$lang['very_high'] = 'Sehr hoch';
+//
+
+$lang['accept'] = 'Accept';
+$lang['cancel'] = 'Abbrechen';
+$lang['yes'] = 'Ja';
+$lang['no'] = 'Nein';
+$lang['privacy_policy'] = 'Datenschutzerklärung';
+$lang['contact'] = 'Kontakt';
+$lang['about_company'] = 'Über die Firma';
+$lang['order_sensor'] = 'Bestellsensor';
+
+$lang['monday'] = 'Montag';
+$lang['tuesday'] = 'Dienstag';
+$lang['wednesday'] = 'Mittwoch';
+$lang['thursday'] = 'Donnerstag';
+$lang['friday'] = 'Freitag';
+$lang['saturday'] = 'Samstag';
+$lang['sunday'] = 'Sonntag';
